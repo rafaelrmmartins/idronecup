27f7587b8b8213fa40a20f8fe53089db8f3181d3
rafaelrmmartins <rafaelrmmartins@gmail.com>
2015-05-01 12:39:04 +0100

    Handler do botão - Método Follow

---
+++ iDronePersonTracking/Mission3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using iDroneCup;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace iDroneExemplos
+
+{
+    public partial class MainForm : Form
+    {
+        public void Mission3()
+        {
+            
+        }
+
+        void Button15Click(object sender, System.EventArgs e)
+        {
+            //Connects and starts drone
+            mDrone.droneLigar();
+            mDrone.iDroneCup_ChangeWifiChannel(2);
+            if (mDrone.droneObterAltitude() == 0)
+                mDrone.droneDescolar();
+            //changes camera
+            mDrone.droneMudarCamara(Drone.DroneCamera.INFERIOR);
+
+
+
+        }
+		
+    }
+}
---
+++ iDronePersonTracking/droneTracker.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by Rafael Martins 
+ * iDroneCup Competition 2015
+ * FEUP team "Black4HawkDown"
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+/*****************CLASS FOR TRACKING LINE POSITION 
+ *****************AND CONSEQUENTLY DRONE MOVEMENT
+*******************/
+
+namespace iDronePersonTracking
+{
+    class droneTracker
+    {
+        private bool forwardMovement;
+        private bool backwardMovement;
+        private bool leftMovement;
+        private bool rightMovement;
+        private bool upMovement;
+        private bool downMovement;
+
+        public droneTracker()
+        {
+
+            forwardMovement = false;
+            backwardMovement = false;
+            leftMovement = false;
+            rightMovement = false;
+            upMovement = false;
+            downMovement = false;
+
+        }
+
+        void followToPoint(Point target, Point imgsize)
+        {
+            //TODO
+        }
+    }
+}
